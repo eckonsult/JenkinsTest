8bee2a4ea7518a79e9b74ae8f03fe37d11fb4320
Peter Lorentzen <peter.lorentzen@ec.se>
2015-11-09 16:48:27 +0100

    add nonworking test

--- ec.JenkinsTest.Tests/ComplexTest.cs
+++ ec.JenkinsTest.Tests/ComplexTest.cs
@@ -15,5 +15,13 @@
             Assert.That(complex.I, Is.EqualTo(4));
         }
 
+        [Test]
+        public void shouldSubtractTwoNumbers() {
+            var c = new Complex { R = 2, I = 2 };
+            var complex = c - c;
+
+            Assert.That(complex.R, Is.EqualTo(4));
+            Assert.That(complex.I, Is.EqualTo(4));
+        }
     }
 }
