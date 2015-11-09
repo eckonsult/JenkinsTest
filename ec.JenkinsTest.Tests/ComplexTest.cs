2c6f00146a8f79fc01bdd2b3007736b8158d320f
Peter Lorentzen <peter.lorentzen@ec.se>
2015-11-09 16:46:34 +0100

    add working test

---
+++ ec.JenkinsTest.Tests/ComplexTest.cs
@@ -0,0 +1,19 @@
+﻿namespace ec.JenkinsTest.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ComplexTest
+    {
+        
+        [Test]
+        public void shouldAddTwoNumbers()
+        {var c = new Complex{R=2,I=2};
+            var complex = c+c;
+
+            Assert.That(complex.R,Is.EqualTo(4));
+            Assert.That(complex.I, Is.EqualTo(4));
+        }
+
+    }
+}
---
+++ ec.JenkinsTest/Complex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ec.JenkinsTest {
+    public class Complex {
+        public int R { get; set; }
+        public int I { get; set; }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+          return new Complex {R = a.R + b.R, I = a.I + b.I};
+        }
+
+        public static Complex operator -(Complex a, Complex b) {
+            return new Complex { R = a.R - b.R, I = a.I + b.I };
+        }
+
+    }
+}
